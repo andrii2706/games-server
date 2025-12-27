da373f8f460dd429cce124806baa04484c6de882
andrii2706 <andriipas200017@gmail.com>
2025-12-27 19:05:58 +0200

    init project

---
+++ service/igdb.service.js
@@ -0,0 +1,17 @@
+const igdbUrl = process.env.TWITCH_IGDB_LINK;
+
+export async function igdbWorker(endpoint, body, clientId, authToken) {
+  const response = await fetch(`${igdbUrl}${endpoint}`, {
+    method: "POST",
+    headers: {
+      "Client-ID": clientId,
+      Authorization: `Bearer ${authToken}`,
+      "Content-Type": "text/plain",
+    },
+    body: body,
+  });
+
+  if (response.status !== 200)
+    throw new Error(`IGDB API request failed with status ${response.status}`);
+  return response.json();
+}
